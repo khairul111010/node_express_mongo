3b86053c9a643147db120077d60015191360ac31
khairul111010 <khairul18103199@gmail.com>
2022-02-08 23:36:36 +0600

    all route

--- backend/routers/goalsRoute.js
+++ backend/routers/goalsRoute.js
@@ -4,5 +4,14 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).json({ message: "GET GOALS" });
 });
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "POST GOALS" });
+});
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `PUT GOALS ${req.params.id}` });
+});
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `DELETE GOALS ${req.params.id}` });
+});
 
 module.exports = router;
