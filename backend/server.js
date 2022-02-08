85f000e98567082f36f542fb454a38d285fdb329
khairul111010 <khairul18103199@gmail.com>
2022-02-08 23:19:27 +0600

    initial server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`listening to ${port}`);
+});
