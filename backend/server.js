414c8d0294cf55bf9b844a431305e25a3f5a32fa
khairul111010 <khairul18103199@gmail.com>
2022-02-08 23:25:42 +0600

    normal send testing

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.get("/api/goals", (req, res) => {
+  res.send("GET GOALS");
+});
+
 app.listen(port, () => {
   console.log(`listening to ${port}`);
 });
