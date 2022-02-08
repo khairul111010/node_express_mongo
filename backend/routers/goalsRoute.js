9bee2a8376520378e82ececd813d91050cf9ec84
khairul111010 <khairul18103199@gmail.com>
2022-02-09 02:23:13 +0600

    route clean up

--- backend/routers/goalsRoute.js
+++ backend/routers/goalsRoute.js
@@ -7,9 +7,7 @@ const {
   deleteGoals,
 } = require("../controllers/goalsController");
 
-router.get("/", getGoals);
-router.post("/", postGoals);
-router.put("/:id", putGoals);
-router.delete("/:id", deleteGoals);
+router.route("/").get(getGoals).post(postGoals);
+router.route("/:id").put(putGoals).delete(deleteGoals);
 
 module.exports = router;
