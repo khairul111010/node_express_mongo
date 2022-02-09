831eaf9e9606ded9932632b13cf4df9232e8565e
Khairul Hasan <khairul18103199@gmail.com>
2022-02-09 14:54:14 +0600

    Error handler modified

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -2,6 +2,10 @@ const getGoals = (req, res) => {
   res.status(200).json({ message: "GET GOALS" });
 };
 const postGoals = (req, res) => {
+  if (!req.body.test) {
+    res.status(400);
+    throw new Error("INVALID TEST VALUE");
+  }
   res.status(200).json({ message: "POST GOALS" });
 };
 const putGoals = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { getGoals } = require("../backend/controllers/goalsController");
-
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 8000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routers/goalsRoute"));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`listening to ${port}`);
