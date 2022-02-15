8a6c67753fbcd69be264632fffbf13c9a9682a29
khairul111010 <khairul18103199@gmail.com>
2022-02-16 01:14:01 +0600

    User registration

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,10 +1,53 @@
-// const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
-// const User = require("../models/userModel");
+//Register a user
+const registerUser = asyncHandler(async (req, res) => {
+  //getting the data
+  const { name, email, password } = req.body;
+  //Empty validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Insert all field are not filled");
+  }
+  //user check
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User exists");
+  }
+  //hashed password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  //creating user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  //user response
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+    });
+  } else {
+    throw new Error("INVALID USER");
+  }
+});
 
-const getUsers = (req, res) => {
+// Login user
+const loginUser = (req, res) => {
   //   const users = await Goal.find();
-  res.json({ messege: "Register" });
+  res.json({ messege: "Login" });
+  //   res.status(200).json(goals);
+};
+
+//Get user data
+const getUser = (req, res) => {
+  //   const users = await Goal.find();
+  res.json({ messege: "User data" });
   //   res.status(200).json(goals);
 };
 
@@ -45,8 +88,7 @@ const getUsers = (req, res) => {
 // });
 
 module.exports = {
-  getUsers,
-  //   postGoals,
-  //   putGoals,
-  //   deleteGoals,
+  registerUser,
+  loginUser,
+  getUser,
 };
--- backend/routers/usersRoute.js
+++ backend/routers/usersRoute.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getUsers,
-//   postGoals,
-//   putGoals,
-//   deleteGoals,
+  registerUser,
+  loginUser,
+  getUser,
 } = require("../controllers/usersController");
 
-router.route("/register").get(getUsers);
+router.route("/register").get(registerUser);
+router.route("/login").get(loginUser);
+router.route("/datauser").get(getUser);
 // router.route("/:id").put(putGoals).delete(deleteGoals);
 
 module.exports = router;
